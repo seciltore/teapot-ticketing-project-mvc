a4bdbcc0dc03bac4cbe18224745ba651d0c1de2d
Cecil T <seciltore.dc@gmail.com>
2020-11-24 14:15:29 -0500

    first commit

---
+++ src/main/java/com/teapot/TeapotTicketingProjectMvcApplication.java
@@ -0,0 +1,13 @@
+package com.teapot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeapotTicketingProjectMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeapotTicketingProjectMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teapot/TeapotTicketingProjectMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teapot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeapotTicketingProjectMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
